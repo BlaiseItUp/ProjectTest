3efb4602d002174485daf4cfa5927d6973254552
Blaise Hackler <blaise.hackler@hobbylobby.com>
2020-09-17 10:27:34 -0500

    did work in working branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+
+            //did some work in my working branch
         }
     }
 }
